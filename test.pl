64bf5bdd6bd1707e56fe43a48c3702c35098b7e1
satishcarya <satish.arya@gmail.com>
2018-05-11 13:30:45 +0000

    commit

--- test.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-print "Hello World\n";
-print qq=Did you say "Hello?"\n=;
-print "Did you say "Hello?"\n";
--- test1.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-print "Hello, Perl\n";
-print ("What is your Name:\n");
-$name=<STDIN>;
-print ("Hello ",$name,"\n");
-
-
